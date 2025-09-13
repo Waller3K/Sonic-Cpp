6b27f22a686e02d7dcb6a8663c5d3952ae7334d2
Waller3K <waltdraws@gmail.com>
2025-09-13 02:01:58 -0500

    Created a basic test program 'Spinning Dorito'

--- src/engine/GameEngine.cpp
+++ src/engine/GameEngine.cpp
@@ -1,35 +1,51 @@
 #include "GameEngine.hpp"
 
 GameEngine::GameEngine()
-{
-
+	: m_isRunning(true)
+{ 
+	Init();
 }
 
 void GameEngine::Init()
 {
-		sf::Window window(sf::VideoMode({1080, 720}), "Sonic C++");
-	
+	m_window.create(sf::VideoMode(m_resolution), "Sonic C++");
 
-
-	while(window.isOpen())
-	{
-		while(const std::optional event = window.pollEvent())
-		{
-			if(event->is<sf::Event::Closed>())
-			{
-				window.close();
-			}
-		}
-	}
-
+	Update();
 }
 
 void GameEngine::Draw()
 {
+	m_window.clear(sf::Color(10,10,10));
+	
+	//A shape for testing
+	sf::CircleShape testShape(50, 3);
+	testShape.setPosition({(float)m_resolution.x/2, (float)m_resolution.y/2});
+	testShape.setFillColor(sf::Color::White);
+	testShape.setRotation(sf::degrees(m_testShapeAngle));
+	
+	m_window.draw(testShape);
 
+	m_window.display();
 }
 
 void GameEngine::Update()
 {
+	while(m_isRunning)
+	{
+
+		m_testShapeAngle += 0.1;
+
+		Draw();
+
+		//Checks to see if the window's x has been pressed.
+		while(const std::optional event = m_window.pollEvent())
+		{
+			if(event->is<sf::Event::Closed>())
+			{
+				m_window.close();
+				m_isRunning = false;
+			}
+		}
+	}
 
 }
--- src/engine/GameEngine.hpp
+++ src/engine/GameEngine.hpp
@@ -4,8 +4,13 @@
 
 class GameEngine
 {
-	sf::Vector2i resolution = {1080, 720};
+	sf::Vector2u		m_resolution 		= {1080, 720};
+	sf::RenderWindow	m_window;
+	bool			m_isRunning 		= false;
+	float			m_testShapeAngle	= 0.0f;
+
 public:
+
 	GameEngine();
 	void Init();
 	void Update();
